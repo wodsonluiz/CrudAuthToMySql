f920727d1bac96daa7f1c1e613278338365d4a9e
Wodson Correia <wodson.correia@bemobi.com.br>
2019-09-11 18:02:37 -0300

    commit initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CrudAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ createMigrations.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `__EFMigrationsHistory` (
+    `MigrationId` NVARCHAR (150) NOT NULL,
+    `ProductVersion` NVARCHAR (32) NOT NULL,
+    PRIMARY KEY (`MigrationId`)
+);
